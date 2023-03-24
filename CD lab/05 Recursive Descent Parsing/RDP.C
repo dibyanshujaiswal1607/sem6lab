600f2cf9e9135c59269a3ab4105c11a4d1fe90de
Dibyanshu Jaiswal <dibyanshujaiswal1607@gmail.com>
2023-03-24 17:57:19 +0530

    RDP

---
+++ CD lab/05 Recursive Descent Parsing/RDP.C
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <conio.h>
+
+int i = 0, f = 0;
+char str[30];
+
+void E();
+void Eprime();
+void T();
+void Tprime();
+void F();
+
+void E() {
+	printf("\nE->TE'");
+	T();
+	Eprime();
+}
+void Eprime() {
+	if(str[i] == '+') {
+		printf("\nE'->+TE'");
+		i++;
+		T();
+		Eprime();
+	} else if((str[i] == ')') || (str[i] == '$'))
+		printf("\nE'->^");
+}
+void T() {
+	printf("\nT->FT'");
+	F();
+	Tprime();
+}
+void Tprime() {
+	if(str[i] == '*') {
+		printf("\nT'->*FT'");
+		i++;
+		F();
+		Tprime();
+	} else if((str[i] == ')') || (str[i] == '+') || (str[i] == '$'))
+		printf("\nT'->^");
+}
+void F() {
+	if(str[i] == 'a') {
+		printf("\nF->a");
+		i++;
+	} else if(str[i] == '(') {
+		printf("\nF->(E)");
+		i++;
+		E();
+		if(str[i] == ')')
+			i++;
+	} else
+		f = 1;
+}
+void main() {
+	int len;
+	clrscr();
+	printf("Enter the string: ");
+	scanf("%s", str);
+	len = strlen(str);
+	str[len] = '$';
+	E();
+	if((str[i] == '$') && (f == 0))
+		printf("\nString successfully parsed!");
+	else
+		printf("\nSyntax Error!");
+	getch();
+}
